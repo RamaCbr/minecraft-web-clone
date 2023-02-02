3f6fe2657f6f2601ebc09973ebda5132d9e8837b
RamaCbr <ramacbr.dev@gmail.com>
2023-02-02 00:31:42 -0300

    feat: :sparkles: Added <Carousel /> component
    Added the <Carousel /> component with its complete CSS styles and basic layout for future reuse in the project.

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,38 @@
+import "./Carousel.scss";
+
+export default function Carousel({
+	data,
+	selectedIndex,
+	increaseIndexHandler,
+	decreaseIndexHandler,
+}) {
+	return (
+		<div className="Carousel">
+			<div className="Carousel-arrow" onClick={decreaseIndexHandler}>
+				-
+			</div>
+			{data.map(({ id, img, title, subtitle }) => {
+				if (selectedIndex === id) {
+					return (
+						<div className="Carousel-item--loaded" key={id}>
+							<img className="Carousel-img" src={img} />
+							<h3 className="Carousel-item-title">{title}</h3>
+							<p className="Carousel-item-subtitle">{subtitle}</p>
+						</div>
+					);
+				} else {
+					return (
+						<div className="Carousel-item" key={id}>
+							<img className="Carousel-img" src={img} />
+							<h3 className="Carousel-item-title">{title}</h3>
+							<p className="Carousel-item-subtitle">{subtitle}</p>
+						</div>
+					);
+				}
+			})}
+			<div className="Carousel-arrow" onClick={increaseIndexHandler}>
+				+
+			</div>
+		</div>
+	);
+}
